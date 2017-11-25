743c1c579aa07184dd7037077e1233344dceed0d
Daniel Wojda <wojdadan@gmail.com>
2017-11-25 16:12:32 +0000

    Example of broadcasting sink

--- src/test/scala/org/danielwoja/akka/sink/OneSourceTwoSinks.scala
+++ src/test/scala/org/danielwoja/akka/sink/OneSourceTwoSinks.scala
@@ -1,15 +1,16 @@
-package org.danielwoja.akka.scala
+package org.danielwoja.akka.sink
 
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.pattern._
-import akka.stream.scaladsl.{GraphDSL, Partition, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, GraphDSL, Partition, RunnableGraph, Sink, Source}
 import akka.stream.{ActorMaterializer, ClosedShape}
 import akka.util.Timeout
-import org.danielwoja.akka.scala.StoreActor.GetStored
+import org.danielwoja.akka.sink.StoreActor.GetStored
 import org.scalatest.concurrent.{Eventually, ScalaFutures}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection._
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -46,6 +47,54 @@ class OneSourceTwoSinks extends FlatSpec with ScalaFutures with Matchers with Ev
     //Then
     eventually {
       storeNumbersActor.ask(GetStored).futureValue shouldBe Seq(1, -1, 2, -2, 3, -3, 4)
+    }
+  }
+
+  "Broadcasting Sink" should "send the same message to both sinks" in {
+    //Given
+    val numbersSource = Source(immutable.Seq(1,2,3))
+    val dbSink: Sink[Int, Future[immutable.Seq[Int]]] = Sink.seq[Int]
+    val emailSink: Sink[Int, Future[immutable.Seq[Int]]] = Sink.seq[Int]
+
+    val graph = RunnableGraph.fromGraph(GraphDSL.create(dbSink, emailSink)((_, _)) {
+      implicit b => (dbSink, emailSink) =>
+      val broadcast = b.add(Broadcast[Int](2, eagerCancel = true))
+
+        numbersSource ~>  broadcast.in
+                          broadcast.out(0) ~> dbSink
+                          broadcast.out(1) ~> emailSink
+
+      ClosedShape
+    })
+
+    //When
+    val (dbSinkMessages, emailSinkMessages) = graph.run()
+
+    //Then
+    dbSinkMessages.futureValue shouldBe Seq(1,2,3)
+    emailSinkMessages.futureValue shouldBe Seq(1,2,3)
+  }
+
+  /**
+    * Simplified Api does not provide materialized values, that is why Sink.seq cannot be used.
+    */
+  it should "send the same message to both sinks when graph is built with simplified API" in {
+    val dbSinkMessages = mutable.ListBuffer[Int]()
+    val emailSinkMessages = mutable.ListBuffer[Int]()
+
+    val dbSink = Sink.foreach[Int](dbSinkMessages += _)
+    val emailSink = Sink.foreach[Int](emailSinkMessages += _)
+    val broadcastingSink = Sink.combine(dbSink, emailSink)(Broadcast[Int](_, eagerCancel = true))
+
+    val stream = Source(immutable.Seq(1, 2, 3)).to(broadcastingSink)
+
+    //when
+    stream.run()
+
+    //Then
+    eventually {
+      dbSinkMessages shouldBe Seq(1,2,3)
+      emailSinkMessages shouldBe Seq(1,2,3)
     }
   }
 }

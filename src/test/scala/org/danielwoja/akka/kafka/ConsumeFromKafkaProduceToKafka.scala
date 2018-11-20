85690454a9e6982a9a4e14a573a69af113824828
Daniel Wojda <wojdadan@gmail.com>
2018-11-20 21:56:36 +0000

    Second version of dropping invalid messages

--- src/test/scala/org/danielwoja/akka/kafka/ConsumeFromKafkaProduceToKafka.scala
+++ src/test/scala/org/danielwoja/akka/kafka/ConsumeFromKafkaProduceToKafka.scala
@@ -16,6 +16,9 @@ import com.ovoenergy.kafka.serialization.avro4s._
 import io.confluent.kafka.schemaregistry.rest.SchemaRegistryRestApplication
 import org.apache.kafka.common.errors.SerializationException
 
+import scala.collection._
+import scala.util.{Failure, Success, Try}
+
 class ConsumeFromKafkaProduceToKafka extends BaseSpec with EmbeddedKafka with EmbeddedSchemaRegistry {
 
   implicit val serializer: StringSerializer = new StringSerializer
@@ -31,7 +34,7 @@ class ConsumeFromKafkaProduceToKafka extends BaseSpec with EmbeddedKafka with Em
     withRunningKafka {
       withEmbeddedSchemaRegistry { () =>
 
-        implicit val userSerializer: Serializer[User] = avroBinarySchemaIdSerializer(schemaRegistryEndpoint , isKey = false, includesFormatByte = true)
+        implicit val userSerializer: Serializer[User] = avroBinarySchemaIdSerializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
         implicit val userDeserializer: Deserializer[User] = avroBinarySchemaIdDeserializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
         implicit val firstNameSerializer: Serializer[FirstName] = avroBinarySchemaIdSerializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
         implicit val firstNameDeserializer: Deserializer[FirstName] = avroBinarySchemaIdDeserializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
@@ -47,8 +50,8 @@ class ConsumeFromKafkaProduceToKafka extends BaseSpec with EmbeddedKafka with Em
         //When
         Consumer.committableSource[String, User](consumerSettings, Subscriptions.topics("test1"))
           .map(msg => (msg.committableOffset, msg.record.value()))
-          .map{case (offset, user) => (offset, FirstName(user.firstName))}
-          .map{case (offset, firstName) => firstName.asProducerMessage(offset) }
+          .map { case (offset, user) => (offset, FirstName(user.firstName)) }
+          .map { case (offset, firstName) => firstName.asProducerMessage(offset) }
           .runWith(Producer.commitableSink(producerSettings))
 
         publishToKafka("test1", User("John", "Wick", 1978))
@@ -63,24 +66,45 @@ class ConsumeFromKafkaProduceToKafka extends BaseSpec with EmbeddedKafka with Em
   it should "skip non-avro messages" in {
     withRunningKafka {
       withEmbeddedSchemaRegistry { () =>
+        def tryDeserializer[T](d: Deserializer[T]): Deserializer[Try[T]] = new Deserializer[Try[T]] {
+          override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = d.configure(configs, isKey)
+          override def deserialize(topic: String, data: Array[Byte]): Try[T] = Try(d.deserialize(topic, data))
+          override def close(): Unit = d.close()
+        }
+
         implicit val userSerializer: Serializer[User] = uncloseable {
-          avroBinarySchemaIdSerializer(schemaRegistryEndpoint , isKey = false, includesFormatByte = true)
+          avroBinarySchemaIdSerializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
         }
         implicit val userDeserializer: Deserializer[User] = avroBinarySchemaIdDeserializer(schemaRegistryEndpoint, isKey = false, includesFormatByte = true)
-        val consumerSettings: ConsumerSettings[String, Array[Byte]] = ConsumerSettings(system, deserializer, new ByteArrayDeserializer())
+        val consumerSettings: ConsumerSettings[String, Try[User]] = ConsumerSettings(system, deserializer, tryDeserializer(userDeserializer))
           .withBootstrapServers(s"localhost:${kafkaConfig.kafkaPort}")
           .withProperty(AUTO_OFFSET_RESET_CONFIG, "earliest")
           .withGroupId("stream")
         val producerSettings: ProducerSettings[String, User] = ProducerSettings(system, serializer, userSerializer)
           .withBootstrapServers(s"localhost:${kafkaConfig.kafkaPort}")
 
-        val decider: Supervision.Decider = _ ⇒ Supervision.Resume
+        val decider: Supervision.Decider = {
+          case e: SerializationException => Supervision.Resume
+          case _                      => Supervision.Stop
+        }
         val mat = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
 
-        Consumer.committableSource[String, Array[Byte]](consumerSettings, Subscriptions.topics("test1"))
-          .map(msg => (msg.committableOffset, userDeserializer.deserialize(msg.record.topic(), msg.record.value())))
-          .map { case (offset, user) => (offset, superImportantBusinessLogic(user))}
-          .map { case (offset, user) => ProducerMessage.Message(new ProducerRecord[String, User]("test2", user), offset)}
+        def exceptionHandler[A, B]: ((A, Try[B])) => immutable.Iterable[(A, B)] = {
+          case (a, Success(b)) =>
+            List((a, b))
+          case (_, Failure(e: SerializationException)) =>
+            println("shit happens...")
+            List()
+          case (_, Failure(e)) =>
+            throw e
+        }
+
+        Consumer.committableSource[String, Try[User]](consumerSettings, Subscriptions.topics("test1"))
+          .map(msg => (msg.committableOffset, msg.record.value()))
+          .mapConcat(exceptionHandler[ConsumerMessage.CommittableOffset, User])
+
+          .map { case (offset, user) => (offset, superImportantBusinessLogic(user)) }
+          .map { case (offset, user) => ProducerMessage.Message(new ProducerRecord[String, User]("test2", user), offset) }
           .runWith(Producer.commitableSink(producerSettings))(mat)
 
         //When
@@ -103,17 +127,21 @@ class ConsumeFromKafkaProduceToKafka extends BaseSpec with EmbeddedKafka with Em
   }
 
   case class User(firstName: String, lastname: String, yearOfBirth: Int)
+
   case class FirstName(value: String)
 
   def uncloseable[T](ser: Serializer[T]): Serializer[T] = new Serializer[T] {
     override def configure(configs: util.Map[String, _], isKey: Boolean): Unit = ser.configure(configs, isKey)
+
     override def serialize(topic: String, data: T): Array[Byte] = ser.serialize(topic, data)
+
     override def close(): Unit = ()
   }
 
 }
 
 case class SchemaRegistryConfig(registryPort: Int, zookeeperUrl: String)
+
 trait EmbeddedSchemaRegistry {
 
 
